1a99a1f72bd68cfe91d6754c57554e05e4a9bbd4
Rahul <imrahul.kgp@gmail.com>
2020-03-25 20:30:34 +0530

    Added CollectionView + CollectionViewCell and DataModel

--- PhotoSearchFlkr/HomeViewController.swift
+++ PhotoSearchFlkr/HomeViewController.swift
@@ -9,12 +9,45 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var viewModel: HomeViewModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.viewModel = HomeViewModel()
+        
     }
-
-
 }
 
+extension HomeViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.PhotosList?.photo.count ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: ImageDisplayCollectionViewCell.self), for: indexPath) as? ImageDisplayCollectionViewCell
+        cell?.setUp(dataModel: viewModel.PhotosList?.photo[indexPath.row])
+        return cell ?? UICollectionViewCell()
+    }
+    
+}
+
+extension HomeViewController : UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 20, left: 0, bottom: 10, right: 0)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let collectionViewWidth = collectionView.bounds.width
+        return CGSize(width: collectionViewWidth/3, height: collectionViewWidth/3)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 20
+    }
+}
---
+++ PhotoSearchFlkr/HomeViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewModel.swift
+//  PhotoSearchFlkr
+//
+//  Created by Rahul Gupta on 25/03/20.
+//  Copyright © 2020 Dunzo. All rights reserved.
+//
+
+import Foundation
+
+class HomeViewModel {
+    var PhotosList: Photos?
+}
