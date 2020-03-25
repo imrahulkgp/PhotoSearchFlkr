549cc61db795addc64e5508512d824eff91fa009
Rahul <imrahul.kgp@gmail.com>
2020-03-25 17:40:54 +0530

    Removed SceneDelegate

--- PhotoSearchFlkr/HomeViewController.swift
+++ PhotoSearchFlkr/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  PhotoSearchFlkr
 //
 //  Created by Rahul Gupta on 25/03/20.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
